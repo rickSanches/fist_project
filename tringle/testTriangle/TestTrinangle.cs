dc6570ff478073a0c65b7b877e90eb2849424b22
rickSanches <joule@ukr.net>
2015-08-18 14:43:45 +0300

    triangle
    triangle square

---
+++ tringle/testTriangle/TestTrinangle.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using tringle;
+
+namespace testTriangle
+{
+    [TestClass]
+    public class TestTrinangle
+    {
+        [TestMethod]
+        public void TestEntered()
+        {
+            
+        }
+    }
+}
